c6aaa2f2180a36ee70e2927954268a846e0732a3
Andreev Artem <jabila.dev@gmail.com>
2022-12-16 17:38:15 +0300

    feat: hist window base

---
+++ Presentation/Sharposhop.AvaloniaUI/ViewModels/HistogramViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Sharposhop.AvaloniaUI.Models;
+
+namespace Sharposhop.AvaloniaUI.ViewModels;
+
+public class HistogramViewModel : ViewModelBase
+{
+    private readonly MainWindowViewModel _mainWindowViewModel;
+
+#pragma warning disable CS8618
+    public HistogramViewModel() { }
+#pragma warning restore CS8618
+
+    public HistogramViewModel(MainWindowViewModel mainWindowViewModel)
+    {
+        _mainWindowViewModel = mainWindowViewModel;
+    }
+
+    public async Task<ColorHistogram[]> GenerateHistograms()
+    {
+        var picture = await _mainWindowViewModel.PictureProvider.ComposePicture();
+        if (_mainWindowViewModel.IsColored)
+        {
+            return new[]
+            {
+                new ColorHistogram(picture, ComponentType.Red, _mainWindowViewModel.Normalizer),
+                new ColorHistogram(picture, ComponentType.Green, _mainWindowViewModel.Normalizer),
+                new ColorHistogram(picture, ComponentType.Blue, _mainWindowViewModel.Normalizer)
+            };
+        }
+
+        return new[] { new ColorHistogram(picture, ComponentType.Red, _mainWindowViewModel.Normalizer) };
+    }
+}
---
+++ Presentation/Sharposhop.AvaloniaUI/Views/Layers/HistogramWindow.axaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ScottPlot;
+using ScottPlot.Avalonia;
+using ScottPlot.Statistics;
+using Sharposhop.AvaloniaUI.ViewModels;
+
+namespace Sharposhop.AvaloniaUI.Views;
+
+public partial class HistogramWindow : ReactiveWindow<HistogramViewModel>
+{
+    public HistogramWindow()
+    {
+        InitializeComponent();
+    }
+
+    public AvaPlot HistogramPlot => this.Find<AvaPlot>("Histogram");
+
+    private async void OnLoad(object? sender, EventArgs e)
+    {
+        var hists = await ViewModel!.GenerateHistograms();
+        var plot = HistogramPlot;
+        var (values, max) = hists[0].GetCounts();
+        
+        var (hist, binEdges) = Common.Histogram(values, 0, max, 1);
+        var leftEdges = binEdges.Take(binEdges.Length - 1).ToArray();
+        
+        plot.Plot.AddBar(hist, leftEdges, Color.Red);
+        plot.Plot.SetAxisLimitsX(0, 255);
+        plot.Plot.SetAxisLimitsY(0, max + 10);
+        plot.Plot.Style(Style.Black);
+        plot.Refresh();
+    }
+    
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
